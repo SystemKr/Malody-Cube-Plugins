bc3cb730d5be9dcb83b5bd6c13dcfbca06b8edec
SystemKr <65324659+SystemKr@users.noreply.github.com>
2024-04-30 18:17:03 +0800

    Update 4-6轨互换_v1.0.lua

--- 4-6轨互换_v1.0.lua
+++ 4-6轨互换_v1.0.lua
@@ -1,19 +1,19 @@
-PluginName='4-6轨互换'
+PluginName='46互换'
 PluginMode=9
 PluginType=0
 PluginRequire='6.0.0'
 
 function Run()
-    Editor:ShowMessage("已开始，请检查谱面以防止出现bug")
+	Editor:ShowMessage("已开始，请检查谱面以防止出现bug")
 	local array = Editor:GetSelectNotes()
 	Editor:StartBatch()
 	for i = 0, array.Length-1 do
 		local column = Editor:GetNoteX(array[i])
 		if column == 3 then
-            Editor:SetNoteX(array[i], 5)
-        elseif column == 5 then
-            Editor:SetNoteX(array[i], 3)
-        end 
+			Editor:SetNoteX(array[i], 5)
+		elseif column == 5 then
+			Editor:SetNoteX(array[i], 3)
+		end 
 	end
 	Editor:FinishBatch()
 end
